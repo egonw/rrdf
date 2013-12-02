f966305f746caf3c0220a7b1dad39bcb8d25d478
Carl Boettiger <cboettig@gmail.com>
2013-12-02 12:28:20 -0800

    Define 'triplestore' class for the Jena model object Addresses issue #12.  This commit merely defines the class to inherit jobjRef class. Coercion is introduced into all functions taking store/model as an argument, which converts the model back to a pure jobjRef class for compatibility with .jcall interface, and a new('triplestore', model) call to all functions returing this object, insuring consistent use of the class.  We can now add traditional S4 methods for the 'triplestore' class such as show, summary, etc.

--- rrdf/R/load.rdf.R
+++ rrdf/R/load.rdf.R
@@ -27,7 +27,7 @@ new.rdf <- function(ontology=TRUE) {
 			"newRdf"
 		)
 	}
-	return(model)
+	return(new("triplestore", model))
 }
 
 load.rdf <- function(filename, format="RDF/XML", appendTo=NULL) {
@@ -40,7 +40,6 @@ load.rdf <- function(filename, format="RDF/XML", appendTo=NULL) {
        		"Lcom/hp/hpl/jena/rdf/model/Model;",
        		"loadRdf", filename, format
     	)
-        return(model)
     } else {
 	    model <- .jcall(
     	    "com/github/egonw/rrdf/RJenaHelper",
@@ -48,6 +47,7 @@ load.rdf <- function(filename, format="RDF/XML", appendTo=NULL) {
        		"loadRdf", filename, format, appendTo
     	)
     }
+  return(new("triplestore", model))
 }
 
 fromString.rdf <- function(rdfContent, format="RDF/XML", appendTo=NULL) {
@@ -71,6 +71,7 @@ fromString.rdf <- function(rdfContent, format="RDF/XML", appendTo=NULL) {
 }
 
 save.rdf <- function(store, filename, format="RDF/XML") {
+  store <- as(store, "jobjRef")
 	formats = c("RDF/XML", "RDF/XML-ABBREV", "N3")
 	if (!(format %in% formats))
 		stop("Formats must be one in: ", formats)
@@ -84,7 +85,9 @@ save.rdf <- function(store, filename, format="RDF/XML") {
 }
 
 combine.rdf <- function(model1, model2) {
-    if (attr(model2, "jclass") != "Lcom/hp/hpl/jena/rdf/model/Model") {
+   model1 <- as(model1, "jobjRef")
+   model2 <- as(model2, "jobjRef")
+   if (attr(model2, "jclass") != "Lcom/hp/hpl/jena/rdf/model/Model") {
         model2 <- .jcast(model2, "com/hp/hpl/jena/rdf/model/Model")
     }
 
@@ -95,6 +98,7 @@ combine.rdf <- function(model1, model2) {
 }
 
 summarize.rdf <- function(model) {
+    model <- as(model, "jobjRef") 
     count <- .jcall(
         "com/github/egonw/rrdf/RJenaHelper",
         "I", "tripleCount", model
@@ -107,6 +111,7 @@ summarize.rdf <- function(model) {
 }
 
 sparql.rdf <- function(model, sparql, rowvarname=NULL) {
+    model <- as(model, "jobjRef") 
     stringMat <- .jcall(
         "com/github/egonw/rrdf/RJenaHelper",
         "Lcom/github/egonw/rrdf/StringMatrix;", "sparql", model, sparql
@@ -156,6 +161,8 @@ add.triple <- function(store,
 	subject="http://example.org/Subject",
 	predicate="http://example.org/Predicate",
 	object="http://example.org/Object") {
+
+   store <- as(store, "jobjRef") 
 	.jcall(
 		"com/github/egonw/rrdf/RJenaHelper",
 		"V",
@@ -169,6 +176,9 @@ add.data.triple <- function(store,
 		predicate="http://example.org/Predicate",
 		data="Value",
 		type=NULL) {
+
+  store <- as(store, "jobjRef") 
+
 	if (is.null(type)) {
 		.jcall(
 			"com/github/egonw/rrdf/RJenaHelper",
@@ -197,7 +207,7 @@ construct.rdf <- function(model, sparql) {
 	if (!is.null(exception)) {
 		stop(exception)
 	}
-	return(newModel)
+	return(new("triplestore", newModel))
 }
 
 construct.remote <- function(endpoint, sparql) {
@@ -223,6 +233,9 @@ add.prefix <- function(store=NULL, prefix=NULL, namespace=NULL) {
     if (is.null(store)) stop("A store must be given.")
     if (is.null(prefix)) stop("A prefix must be given.")
     if (is.null(namespace)) stop("A namespace must be given.")
+## FIXME Why are we giving defaults if values are required?? 
+
+  store <- as(store, "jobjRef") 
 
 	.jcall(
 		"com/github/egonw/rrdf/RJenaHelper",
