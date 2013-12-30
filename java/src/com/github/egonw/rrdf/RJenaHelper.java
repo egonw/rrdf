92bc2569562236b7f2fcb49d0dd5f27e8e9c8aac
Egon Willighagen <egon.willighagen@gmail.com>
2013-12-30 14:14:48 +0100

    Make a default model by, ummm, default

--- java/src/com/github/egonw/rrdf/RJenaHelper.java
+++ java/src/com/github/egonw/rrdf/RJenaHelper.java
@@ -82,7 +82,7 @@ public class RJenaHelper {
   }
 
   public static Model fromString(String rdfContent, String format) throws Exception {
-          return fromString(rdfContent, format, ModelFactory.createOntologyModel());
+          return fromString(rdfContent, format, ModelFactory.createDefaultModel());
   }
 
   public static Model fromString(String rdfContent, String format, Model appendTo) throws Exception {
